8f3144752e27681f5d53743b4838f7575e0a9d2a
Artem Barhanov <darakon92@gmail.com>
2017-08-20 19:28:34 +0300

    example of connection to MySQL

--- code/index.php
+++ code/index.php
@@ -1,2 +1,11 @@
 <?php
-phpinfo();
+// phpinfo();
+
+$dsn = 'mysql:host=dbMysql;dbname=test;charset=utf8';
+$opt = array(
+	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+$_pdo = new PDO($dsn, 'root', 'example', $opt);
+var_dump($_pdo->errorCode());
+exit;
